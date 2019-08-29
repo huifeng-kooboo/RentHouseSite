0c7c9fccdc137b9497ff5c45aafcdd70a588c138
duhuifeng <gisdoing@gmail.com>
2019-08-29 09:10:09 +0800

    简单实现前后分离的Vue+Django搭建
    简单实现前后分离的Vue+Django搭建

--- renthouse/renthouse/settings.py
+++ renthouse/renthouse/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'renthouse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['vuefront/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR,'vuefront/dist/static'))
 
 # config media setting
 MEDIA_URL = '/media/'
--- renthouse/renthouse/urls.py
+++ renthouse/renthouse/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import url,include
 
 from rest_framework.routers import DefaultRouter,SimpleRouter
 from rest_framework.authtoken import views
+from django.views.generic.base import TemplateView
 
 import xadmin
 
@@ -21,6 +22,7 @@ urlpatterns = [
     path('api-token-auth/',views.obtain_auth_token),
     #path('admin/', admin.site.urls),
     path('xadmin/',xadmin.site.urls),
-    url(r'^',include(router.urls)),
+   # url(r'^',include(router.urls)),
+    path(r'', TemplateView.as_view(template_name="index.html")),
     url(r'^api-auth/',include('rest_framework.urls',namespace='rest_framework'))
 ]
