f46edc60647fae926e246c23eed1da93c4819a13
duhuifeng <gisdoing@gmail.com>
2019-09-03 20:08:25 +0800

    添加了上传图片的方法 晚上完善
    添加上传图片的方法，晚上完善

--- renthouse/vuefront/src/components/projects/AddHouse.vue
+++ renthouse/vuefront/src/components/projects/AddHouse.vue
@@ -5,12 +5,29 @@
     <!--先放置常用导航栏模块-->
     <VNavbar></VNavbar>
     <h2>添加房源</h2>
-    <ul>
-    <!--列表渲染测试-->
-    <li v-for="item in items">
-      {{item.message}}
-    </li>
-    </ul>
+    <p>上传图片</p>
+    <!-- el-upload 上传
+    limit:限制上传照片数量：5
+    multiple：表示允许上传多张图片
+    accept:允许上传的图片类型
+
+    -->
+    <el-upload
+      class="upload_housejpg"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      accept="image/jpeg,image/gif,image/png"
+      :before-upload="onBeforeUpload"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :on-success = 'onSuccessUpload'
+      :file-list="fileList"
+      multiple
+      :limit="5"
+      :on-exceed="uploadExceed"
+      list-type="picture">
+      <el-button size="small" type="primary">上传房源图片</el-button>
+      <div slot="tip" class="el-upload__tip">格式允许jpg/png,最多允许上传5张图</div>
+    </el-upload>
   </div>
 </template>
 
@@ -20,10 +37,8 @@
         name: "AddHouse",
       data(){
           return{
-            items:[
-              {message:'foo'},
-              {message:'fff'}
-            ]
+            //绑定相应的数据信息
+            fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
           }
       },
        components:{
@@ -37,6 +52,35 @@
       },
       methods:{
           //存放各种方法类
+        //此处存放上传图片各种操作
+        handleRemove(file, fileList) {
+          console.log(file, fileList);
+        },
+        handlePreview(file) {
+          console.log(file);
+        },
+        uploadExceed(){
+          //超过5张图限制处理
+          alert("上传数量超出限制，最多只允许5张图，联系QQ：942840260");
+        },
+        onBeforeUpload(file){
+          //上传前，检查数据类型
+          const isIMAGE = file.type === 'image/jpeg'||'image/gif'||'image/png';
+          const isLt1M = file.size / 1024 / 1024 < 1; //大小小于1M
+
+          if (!isIMAGE) {
+            alert('只允许上传图片');
+          }
+          if (!isLt1M) {
+            alert('上传照片大小需要小于1M');
+          }
+          return isIMAGE && isLt1M;
+        },
+        onSuccessUpload(file,fileList){
+          alert("恭喜上传成功"+file.name);
+        },
+
+        //功能
       }
     }
 </script>
