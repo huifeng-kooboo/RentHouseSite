c6f45b3c99187532c82ecc6b568a2724acd48457
huifeng-kooboo <gisdoing@gmail.com>
2019-09-12 10:59:10 +0800

    添加费用清单假数据模板
    添加费用清单假数据

--- renthouse/vuefront/src/components/projects/FeeList.vue
+++ renthouse/vuefront/src/components/projects/FeeList.vue
@@ -3,7 +3,23 @@
     <div>
       <!--添加常规导航栏-->
       <VNavbar></VNavbar>
-      <h2>费用清单</h2>
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+          prop="price_type"
+          label="类型"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="price"
+          label="价格"
+          width="180">
+        </el-table-column>
+      </el-table>
+
+      <el-button type="primary">立即支付</el-button>
     </div>
 </template>
 
@@ -13,6 +29,31 @@
         name: "FeeList",
       components:{
           VNavbar:Navbar,
+      },
+      data(){
+        return{
+          //tableData:费用清单列表
+          tableData: [{
+            price_type: '月租',
+            price: '800元',
+          }, {
+            price_type: '电费',
+            price: '80元',
+          }, {
+            price_type: '物业费',
+            price: '90元',
+          }, {
+            price_type: '水费',
+            price: '90元',
+          }, {
+            price_type: '网费',
+            price: '90元',
+          },{
+            price_type:'合计',
+            price:'10011元',
+          }],
+
+        }
       },
     }
 </script>
