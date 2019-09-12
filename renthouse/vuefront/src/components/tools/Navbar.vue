1c5129afc27f37cc68a27e4a6f1a813c4a1fc993
huifeng-kooboo <gisdoing@gmail.com>
2019-09-12 09:26:17 +0800

    添加修改密码前端
    添加修改密码前端的功能

--- renthouse/vuefront/src/components/tools/Navbar.vue
+++ renthouse/vuefront/src/components/tools/Navbar.vue
@@ -7,7 +7,7 @@
     <el-submenu index="2">
       <template slot="title">个人中心</template>
       <el-menu-item index="2-1">个人信息设置</el-menu-item>
-      <el-menu-item index="2-2">修改密码</el-menu-item>
+      <el-menu-item index="2-2"><a id="a_modify" href="/modifypassword">修改密码</a></el-menu-item>
       <el-menu-item index="2-3"><a id="a_login" href="/login">用户登录</a></el-menu-item>
       <el-menu-item index="2-3"><a href="/register">用户注册</a></el-menu-item>
       <el-menu-item index="2-4">退出登录</el-menu-item>
--- renthouse/vuefront/src/components/users/ModifyPassword.vue
+++ renthouse/vuefront/src/components/users/ModifyPassword.vue
@@ -2,14 +2,64 @@
   <!--主要用于忘记密码和修改密码 用一个模板： 原因：
   不能找回密码。
   -->
-    <div>
-      <h3>修改密码模块</h3>
+    <div id="div_modify">
+      <VNavbar></VNavbar>
+      <el-row :gutter="30">
+        <el-col :span="3">
+          请输入手机号：
+        </el-col>
+        <el-col :span="6">
+          <el-input v-model="input_phone" placeholder="手机号"></el-input>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3">
+          请输入手机验证码：
+        </el-col>
+        <el-col :span="6">
+          <el-input v-model="input_verify" placeholder="手机验证码"></el-input>
+        </el-col>
+        <el-col :span="6">
+          <el-button type="primary">发送验证码</el-button>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3">
+          请输入新密码：
+        </el-col>
+        <el-col :span="6">
+          <el-input v-model="input_newpsd" placeholder="请输入新密码"></el-input>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3">
+          请确认新密码：
+        </el-col>
+        <el-col :span="6">
+          <el-input v-model="input_repsd" placeholder="请确认新密码"></el-input>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-button type="primary">提交</el-button>
+      </el-row>
     </div>
 </template>
 
 <script>
+  import Navbar from '../tools/Navbar'
     export default {
-        name: "ModifyPassword"
+        name: "ModifyPassword",
+      components:{
+          VNavbar:Navbar,
+      },
+      data(){
+          return{
+            input_phone:'',
+            input_verify:'',
+            input_newpsd:'',
+            input_repsd:'',
+          }
+      }
     }
 </script>
 
