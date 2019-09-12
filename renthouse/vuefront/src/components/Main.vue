60d644b361414763cf1ed2fe767fd464622080e8
huifeng-kooboo <gisdoing@gmail.com>
2019-09-12 15:46:02 +0800

    前端主页添加走马灯功能
    前端主页添加走马灯功能

--- renthouse/vuefront/src/components/Main.vue
+++ renthouse/vuefront/src/components/Main.vue
@@ -4,6 +4,14 @@
       <!-- VNavbar 引入导航栏模块-->
       <VNavbar></VNavbar>
       <el-input id="username" class="username" v-model="input_username" suffix-icon="el-icon-user" placeholder = "请输入用户名"></el-input>
+
+      <!--添加走马灯，用于展示更新信息-->
+      <el-carousel trigger="click" height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3 class="small">{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+
       <VHouse></VHouse>
     </div>
 </template>
@@ -48,4 +56,21 @@
 </script>
 
 <style scoped>
+
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+
 </style>
