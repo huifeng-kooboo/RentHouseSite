49bf7cb942bba160ec9d01fb1c904586ce2b8048
ytouch <gisdoing@gmail.com>
2019-09-15 08:08:06 +0800

    修改跳转路由和显示界面
    简单的修改了跳转路由和显示的逻辑

--- renthouse/vuefront/src/components/projects/HouseDetail.vue
+++ renthouse/vuefront/src/components/projects/HouseDetail.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
     <VNavbar></VNavbar>
-    <h2>厦门第五房</h2>
-    <!--房源图片-->
-    <el-avatar shape="square" :size="size" :src="squareUrl"></el-avatar>
-    <h3>房屋简单介绍如下：这房子很好看</h3>
-    <h3>当前房价：900元</h3>
-    <h3>房屋位置：广东省深圳</h3>
-    <h3>手机号：13824464121</h3>
-    <h3>房东名：正吃哈</h3>
-    <el-button type="primary">联系房东</el-button>
+
+    <div id="div_brief">
+      <el-tag >房屋名称：</el-tag>
+      <el-tag type="success">{{ house_title }}</el-tag>
+      <el-divider></el-divider>
+      <el-image :src="house_images" style="width: 300px;height: 300px"></el-image>
+      <el-divider></el-divider>
+      <el-tag>简单介绍：</el-tag>
+      <el-tag type="success">{{ basic_interviews }}</el-tag>
+      <el-divider></el-divider>
+      <el-tag>房屋价格：</el-tag>
+      <el-tag type="success">{{ house_price }}</el-tag>
+      <el-divider></el-divider>
+      <el-tag>房屋住址：</el-tag>
+      <el-tag type="success">{{ house_position }}</el-tag>
+      <el-divider></el-divider>
+      <el-tag>联系方式：</el-tag>
+      <el-tag type="success">{{ connect_phone }}</el-tag>
+      <el-divider></el-divider>
+      <el-tag> 业主姓名：</el-tag>
+      <el-tag type="success">{{ renter_name }}</el-tag>
+      <el-divider></el-divider>
+      <el-button type="primary">立即联系</el-button>
+    </div>
+
   </div>
 
 </template>
@@ -24,35 +40,40 @@
         data(){
             return{
                 squareUrl:'https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png',
+                house_title:'',
+                house_images:'https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png',
+                basic_interviews:'',
+                house_price:'',
+                house_position:'',
+                connect_phone:'',
+
             }
         },
         mounted() {
+            let that = this;
             //处理路由
             let house_title = this.$route.params.housetitle; //获取房源名称
             console.log(house_title);
+            that.house_title = house_title;
             let json_data = {'house_title':house_title};
             //发送get请求
             this.$axios.get('http://localhost:8080/api/housedetail/', {
                 params: json_data
             })
                 .then(function (response) {
-                    console.log(response);
+                    if (response.status == 200)
+                    {
+                        that.house_images = response.data['results'][0]['house_images'];
+                        that.basic_interviews = response.data['results'][0]['basic_interviews'];
+                        that.house_price = response.data['results'][0]['house_price'];
+                        that.house_position = response.data['results'][0]['house_position'];
+                        that.connect_phone = response.data['results'][0]['connect_phone'];
+                        that.renter_name = response.data['results'][0]['renter_name'];
+                    }
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
-            //发送get请求获取所有信息
-            // this.$axios(
-            //     {
-            //         url:'http://localhost:8080/api/housedetail/',
-            //         method:'get',
-            //         params :JSON.stringify(json_data),
-            //     }
-            // ).then(
-            //     function(res){
-            //         console.log(res.data);
-            //     }
-            // );
         }
     }
 </script>
--- renthouse/vuefront/src/components/projects/HousesInfo.vue
+++ renthouse/vuefront/src/components/projects/HousesInfo.vue
@@ -39,7 +39,7 @@
                 console.log("当前按钮的id为：");
                 console.log(this.house_infos[index]['house_images']);
                 console.log(index);
-                window.location.href='/login';
+                window.location.href='/housedetail/'+this.house_infos[index]['house_title']; //跳转到指定url
             },
       },
         created(){
