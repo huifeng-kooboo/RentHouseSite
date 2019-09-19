88080115100f9345286679b245669423680313c0
ytouch <gisdoing@gmail.com>
2019-09-20 00:24:57 +0800

    完善了租房管理的方法
    解决了租房管理用户的显示问题，现在还剩那个编辑和管理

--- renthouse/vuefront/src/components/projects/LandlordHouse.vue
+++ renthouse/vuefront/src/components/projects/LandlordHouse.vue
@@ -10,7 +10,7 @@
         <!--租户名-->
         <el-table-column
           label="租户名"
-          width="100">
+          width="130">
           <template slot-scope="scope">
             <span style="margin-left: 10px">{{ scope.row.renter_name }}</span>
           </template>
@@ -41,7 +41,7 @@
         <!--入租租金-->
         <el-table-column
           label="本月租金(元/月)"
-          width="130">
+          width="100">
           <template slot-scope="scope">
             <span style="margin-left: 10px">{{ scope.row.rent_fee }}</span>
           </template>
@@ -50,12 +50,58 @@
         <!--电费-->
         <el-table-column
           label="电费(元/月)"
-          width="130">
+          width="100">
           <template slot-scope="scope">
             <span style="margin-left: 10px">{{ scope.row.electric_fee }}</span>
           </template>
         </el-table-column>
 
+        <!--水费-->
+        <el-table-column
+          label="水费(元/月)"
+          width="100">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.water_fee }}</span>
+          </template>
+        </el-table-column>
+
+        <!--网费-->
+        <el-table-column
+          label="网费(元/月)"
+          width="100">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.net_fee }}</span>
+          </template>
+        </el-table-column>
+
+        <!--钥匙数量-->
+        <el-table-column
+          label="钥匙数量"
+          width="100">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.key_num }}</span>
+          </template>
+        </el-table-column>
+
+        <!--是否有空调-->
+        <el-table-column
+          label="是否有空调"
+          width="100">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.air_num }}</span>
+          </template>
+        </el-table-column>
+
+        <!--是否有洗衣机-->
+        <el-table-column
+          label="是否有洗衣机"
+          width="100">
+          <template slot-scope="scope">
+            <span style="margin-left: 10px">{{ scope.row.washer_num }}</span>
+          </template>
+        </el-table-column>
+
+        <!--编辑功能对应修改 删除功能向后端发起delete请求即可-->
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button
@@ -82,39 +128,58 @@
       },
       mounted() {
           //这边发送请求获取数据
-      }
-      ,
+          let cur_headers = {'Authorization':"JWT " + localStorage.getItem('token')};//
+          let that = this;
+          this.$axios({
+              url:'api/allrenters/',
+              method:'get',
+              headers:cur_headers //添加token认证千万不能忘
+          }).then(function (res) {
+              //console.log(res.data);
+              let cur_count = res.data['count'];
+              if (cur_count < 1)
+              {
+                  this.$alert('当前您未添加租户房源！');
+                  return;
+              }
+              let i = 0;
+              for (;i< cur_count;i++)
+              {
+                  let json_data = {'renter_name':res.data['results'][i]['tenant'],'date':res.data['results'][i]['rental_time'],
+                  'address':res.data['results'][i]['rental_address'],'rent_fee':res.data['results'][i]['rent_fee'],
+                  'electric_fee':res.data['results'][i]['electric_fee'],'water_fee':res.data['results'][i]['water_fee'],
+                  'net_fee':res.data['results'][i]['net_fee'],'key_num':res.data['results'][i]['key_num'],
+                  'air_num':res.data['results'][i]['is_air'],'washer_num':res.data['results'][i]['is_washer']};
+                  that.tableData.unshift(json_data);
+              }
+              that.tableData.pop();
+          }).catch(function (res) {
+              //处理异常 不擅长
+          });
+      },
       data() {
         return {
           tableData: [{
             renter_name:'小黄',
             date: '2016-05-02',
-            name: '王小虎',
             address: '上海市普陀区金沙江路 1518 弄',
             rent_fee:1999,
-            electric_fee:12
-          }, {
-            renter_name:'小黄',
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄',
-            rent_fee: 20000,
-            electric_fee:12
-          }, {
-            renter_name:'小黄',
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-            rent_fee: 200,
-            electric_fee:12
+            electric_fee:12,
+              water_fee:14,
+              net_fee:20,
+              key_num:1,
+              air_num :1,
+              washer_num:1,
           }]
         }
       },
       methods: {
         handleEdit(index, row) {
+            //处理编辑问题
           console.log(index, row);
         },
         handleDelete(index, row) {
+            //处理删除问题
           console.log(index, row);
         }
       }
