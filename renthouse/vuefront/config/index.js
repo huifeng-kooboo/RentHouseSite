92db26afec1fcceb33a53b68d2d19e0547ae52fc
huifeng-kooboo <gisdoing@gmail.com>
2019-10-07 15:47:08 +0800

    修改
    修改测试

--- renthouse/vuefront/config/index.js
+++ renthouse/vuefront/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {
       // proxy all requests starting with /api to jsonplaceholder
       '/api': {
-        target: 'http://localhost:8000', //跨域访问的地址
+        target: 'http://49.234.6.143:8070', //跨域访问的地址
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
@@ -22,7 +22,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '49.234.6.143', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
